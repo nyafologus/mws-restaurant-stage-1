f052fde3b5399f5c1fb75120607d7a160a02cffc
Szofi <nyafologus@gmail.com>
2018-07-14 16:31:59 +0200

    OFFLINE Setting up ServiceWorker

--- js/swregister.js
+++ js/swregister.js
@@ -1,11 +1,14 @@
-//checks if the browser supports Service Worker
-if(navigator.serviceWorker){
-  // if it does, registeres the Service Worker
-  navigator.serviceWorker.register('sw.js').then(function(reg){
-    //if registration resolves
-    console.log('Service Worker registered');
-  }).catch (function (err){
-    //if registration failes
-    console.log ('Something went wrong: No Service Worker');
-  });
+//Checks if Browser supports Service Worker 
+if("serviceWorker" in navigator){
+  // Sets up Service Worker
+  navigator.serviceWorker
+  	.register("sw.js")
+  	.then(reg => {
+    	//In case of successful registration
+    	console.log("Service Worker registered");
+  	})
+  	.catch(error => {
+    	//In case registration fails
+    	console.log("Something went wrong: Service Worker Registration Failed");
+  	});
 };
